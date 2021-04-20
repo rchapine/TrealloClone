e9c00970917abf1549a698d23401cd661951bb5c
Rafael Chapine <rchapine@gmail.com>
2021-04-19 20:55:41 -0400

    cominting tree branches

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Components } from "react";
+import TrelloList from "./TrelloList";
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+      <TrelloList />
+    </div>
+  );
+}
+
+export default App;
